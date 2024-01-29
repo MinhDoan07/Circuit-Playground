97abe44a7a278309e3b17132c2bc979f4bb4a2cd
MinhDoan07 <137840310+MinhDoan07@users.noreply.github.com>
2024-01-29 08:11:50 -0800

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,9 +1,1 @@
-
-from adafruit_circuitplayground import cp
-wav = "MEOW.wav"
-while True:
-    if cp.button_a:
-        cp.play_file(wav)
-    if cp.button_b:
-        cp.play_file(wav)
-
+print("Hello, World")
