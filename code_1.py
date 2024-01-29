14198996e151d0eadd7a9c51b0b08fc608a3060c
MinhDoan07 <137840310+MinhDoan07@users.noreply.github.com>
2024-01-29 08:59:43 -0800

    CPX-CPB-CircleNeoPixel.py

--- code_1.py
+++ code_1.py
@@ -1,9 +1,14 @@
-
-from adafruit_circuitplayground import cp
-wav = "MEOW.wav"
+import time
+import board
+import neopixel
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
+i = 0
+pixels.fill((100, 100, 100))
 while True:
-    if cp.button_a:
-        cp.play_file(wav)
-    if cp.button_b:
-        cp.play_file(wav)
-
+    pixels[i] = ((100, 30, 20))
+    pixels[i - 1] = ((100, 100, 100))
+    i = i + 1
+    time.sleep(0.05)
+    if i == 10 :
+        i = 0
+        pixels[9] = ((0, 0, 0))
