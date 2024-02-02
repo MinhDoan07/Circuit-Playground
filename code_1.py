b52cc39cbeb69fbb0fb6a5986beb15c5de26242d
MinhDoan07 <137840310+MinhDoan07@users.noreply.github.com>
2024-02-02 09:46:54 -0800

    Add files via upload

--- code_1.py
+++ code_1.py
@@ -1,14 +1,11 @@
 import time
 import board
 import neopixel
+import random
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
-i = 0
-pixels.fill((100, 100, 100))
 while True:
-    pixels[i] = ((100, 30, 20))
-    pixels[i - 1] = ((100, 100, 100))
-    i = i + 1
-    time.sleep(0.05)
-    if i == 10 :
-        i = 0
-        pixels[9] = ((0, 0, 0))
+    i = random.randint(0, 9)
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    pixels[i] = ((r, g, b))
